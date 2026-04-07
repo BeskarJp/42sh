20a80c4c0a8acbce7125df5fea123b0b22d9f503
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-07 22:32:46 +0200

    [L.J][fix/42sh-project] Style: Improve the shell's prompt line

--- src/shell/shell_prompt_line.c
+++ src/shell/shell_prompt_line.c
@@ -23,6 +23,28 @@ void print_username(char **env)
 }
 
 /**
+ * @brief Finds the HOME in env and compare to getcwd line for replace with ~
+ *
+ * @param way Exactly position of user
+ * @param env Array of the environment
+ */
+void print_shorter_path(char *way, char **env)
+{
+    char *home = find_word_in_env(env, "HOME");
+    int home_len = 0;
+
+    if (home != NULL) {
+        home_len = my_strlen(home);
+        if (my_strncmp(way, home, home_len) == 0) {
+            write(1, "~", 1);
+            my_putstr(way + home_len);
+            return;
+        }
+    }
+    my_putstr(way);
+}
+
+/**
  * @brief Displays the shell prompt with user info and current directory
  *
  * @param env Array of the environment
@@ -39,7 +61,7 @@ void print_shell_line(char **env)
         write(1, RESET, my_strlen(RESET));
         write(1, ":", 1);
         write(1, PURPLE, my_strlen(PURPLE));
-        my_putstr(way);
+        print_shorter_path(way, env);
         write(1, RESET, my_strlen(RESET));
         write(1, " $> ", 4);
     }
