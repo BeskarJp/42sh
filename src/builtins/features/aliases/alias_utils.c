36bfaf8cbab10a74ac0404b6f067194ba5b2eda4
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-09 19:01:40 +0200

    [L.J][feature/aliases] Feat: Add functions utils for aliases feature

---
+++ src/builtins/features/aliases/alias_utils.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** Alias' utils for shell
+*/
+
+#include "shell.h"
+
+alias_t *find_alias_by_name(alias_t *aliases, char *name)
+{
+    alias_t *temp = aliases;
+
+    while (temp) {
+        if (my_strcmp(temp->name, name) == 0)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+void add_alias(shell_t *shell, char *name, char *command)
+{
+    alias_t *correlation = find_alias_by_name(shell->aliases, name);
+    alias_t *new_node;
+
+    if (correlation) {
+        free(correlation->command);
+        correlation->command = my_strdup(command);
+        return;
+    }
+    new_node = malloc(sizeof(alias_t));
+    if (!new_node)
+        return;
+    new_node->name = my_strdup(name);
+    new_node->command = my_strdup(command);
+    new_node->next = shell->aliases;
+    shell->aliases = new_node;
+}
