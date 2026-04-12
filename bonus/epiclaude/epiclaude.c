faa7df71b187c322e49660b6fb6adce3fda66cd5
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-12 17:32:34 +0200

    [L.J][bonus/easter-egg] Fix: Fix somes little things in epiclaude

--- bonus/epiclaude/epiclaude.c
+++ bonus/epiclaude/epiclaude.c
@@ -17,16 +17,16 @@ void start_claude(shell_t *shell)
     while (1) {
         my_printf("%sEpiClaude >%s ", ORANGE, RESET);
         read_bytes = getline(&line, &len, stdin);
-        if (read_bytes == -1 || my_strcmp(line, "quit\n") == 0 ||
-            my_strcmp(line, "exit\n") == 0)
-            break;
         if (line[read_bytes - 1] == '\n')
             line[read_bytes - 1] = '\0';
+        if (read_bytes == -1 || my_strcmp(line, "quit") == 0 ||
+            my_strcmp(line, "exit") == 0 || my_strcmp(line, "exit") == 0)
+            break;
         if (line[0] != '\0')
             handle_input(line);
     }
-    ia_style_text_writer("Bye Bye ! Returning to 42sh", SLOW);
-    ia_style_text_writer(" ...\n", ULTRA_SLOW);
+    ia_style_text_writer("\nBye Bye ! Returning to 42sh", SLOW);
+    ia_style_text_writer(" ...\n\n", ULTRA_SLOW);
     if (line)
         free(line);
 }
