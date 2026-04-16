b47000d4935bdefd0861e227c31f46c3630522ef
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-16 14:44:40 +0200

    [L.J][fix/42sh-project] Fix: Fix the 'path handling' tests in 42sh

--- src/environment/find_pathway.c
+++ src/environment/find_pathway.c
@@ -72,11 +72,13 @@ char *find_command_path(char *command, char **env)
     char *path_copy = NULL;
     char *path_way = NULL;
 
-    if (env == NULL)
+    if (env == NULL  || command == NULL)
         return NULL;
-    if (command != NULL && (command[0] == '/' || command[0] == '.')
-        && access(command, X_OK) == 0)
-        return my_strdup(command);
+    if (strchr(command, '/') != NULL) {
+        if (access(command, F_OK) == 0)
+            return my_strdup(command);
+        return NULL;
+    }
     if (path_line_in_env == NULL)
         return NULL;
     path_copy = my_strdup(path_line_in_env);
