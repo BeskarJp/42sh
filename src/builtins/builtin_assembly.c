ddf697ff96bf487cadd3d58f95d9bb4122bc16aa
Minet Hugo <hugo-m@epitech.eu>
2026-04-17 14:41:47 +0200

    [H.M][bonus/Travis] Feat: add ascii art Travis Scott --> cactus

--- src/builtins/builtin_assembly.c
+++ src/builtins/builtin_assembly.c
@@ -7,6 +7,30 @@
 
 #include "shell.h"
 
+void bonus_builtin_ascii_art_cactus(void)
+{
+    my_printf("                /||\\\n");
+    my_printf("                ||||\n");
+    my_printf("                ||||\n");
+    my_printf("                |||| /|\\\n");
+    my_printf("           /|\\  |||| |||\n");
+    my_printf("           |||  |||| |||\n");
+    my_printf("           |||  |||| |||\n");
+    my_printf("           |||  |||| d||\n");
+    my_printf("           |||  |||||||/\n");
+    my_printf("           ||b._||||~~'\n");
+    my_printf("           \\||||||||\n");
+    my_printf("            `~~~||||\n");
+    my_printf("                ||||\n");
+    my_printf("                ||||\n");
+    my_printf("~~~~~~~~~~~~~~~~||||~~~~~~~~~~~~~~\n");
+    my_printf("  \\/..__..--  . |||| \\/  .  ..\n");
+    my_printf("\\/         \\/ \\/    \\/\n");
+    my_printf("        .  \\/              \\/    .\n");
+    my_printf(". \\/             .   \\/     .\n");
+    my_printf("      T R A V I $  S C O T T     \n");
+}
+
 /**
  * @brief Executes a bonus builtin among EpiClaude if recognized
  *
@@ -131,5 +155,9 @@ int builtin_assembly(shell_t *shell)
     }
     if (builtin_exec(shell) == 1)
         return 1;
+    if (my_strcmp(shell->arg_col[0], "cactus") == 0) {
+        bonus_builtin_ascii_art_cactus();
+        return 1;
+    }
     return 0;
 }
