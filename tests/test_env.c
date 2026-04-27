6f61eec2eaaa02526d085441195c8913f9c71b43
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-27 19:03:55 +0200

    [L.J][tests/units-tests] Feat: Add env tests

---
+++ tests/test_env.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** Tests env functions
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "shell.h"
+
+Test(env, copy_env_integrity)
+{
+    char *env[] = {"PATH=/bin", "USER=hugoat", NULL};
+    char **copy = copy_environment(env);
+
+    cr_assert_str_eq(copy[0], "PATH=/bin");
+    cr_assert_str_eq(copy[1], "USER=hugoat");
+    cr_assert_null(copy[2]);
+    free_array(copy);
+}
+
+Test(env, find_var_value)
+{
+    char *env[] = {"HOME=/home/user", "SHELL=42sh", NULL};
+
+    cr_assert_str_eq(find_word_in_env(env, "HOME"), "/home/user");
+    cr_assert_null(find_word_in_env(env, "UNKNOWN"));
+}
+
+Test(env, create_env_line)
+{
+    char *line = add_line_in_env("MY_VAR", "42");
+
+    cr_assert_str_eq(line, "MY_VAR=42");
+    free(line);
+}
