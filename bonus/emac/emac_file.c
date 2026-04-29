7140e4cc1c8acb821043a7017f4ed78a8b982c93
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-04-29 23:43:14 +0200

    [L.J][bonus/emac] Style: Rename file 'emac_utils.c' to 'emac_file.c'

---
+++ bonus/emac/emac_file.c
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2026
+** 42sh
+** File description:
+** Function linked to file
+*/
+
+#include "shell.h"
+
+char *load_emac_file(char *pathline)
+{
+    char *file_buffer = malloc(sizeof(char) * 4096);
+    int fd = open(pathline, O_RDONLY);
+    int size = 0;
+
+    if (!file_buffer)
+        return NULL;
+    memset(file_buffer, 0, 4096);
+    if (fd != -1) {
+        size = read(fd, file_buffer, 4095);
+        if (size < 0)
+            size = 0;
+        file_buffer[size] = '\0';
+        close(fd);
+    }
+    return file_buffer;
+}
+
+void save_emac_file(emac_t *editor)
+{
+    int fd = open(editor->pathline, O_WRONLY | O_CREAT | O_TRUNC, PERM_NORM);
+
+    if (fd == -1) {
+        mvprintw(0, 0, "Error: Cannot save file!");
+        return;
+    }
+    write(fd, editor->file_buffer, editor->len);
+    close(fd);
+}
