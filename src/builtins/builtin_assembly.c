f30a8cdfe95eff430b86ddf4fbbe7c6ed89ff944
Laouenan Jupon <laouenan1.jupon@epitech.eu>
2026-05-07 23:36:33 +0200

    [L.J][fix/42sh-project] Style: Fix builtin and README.md

--- src/builtins/builtin_assembly.c
+++ src/builtins/builtin_assembly.c
@@ -30,7 +30,8 @@ int bonus_builtin_exec(shell_t *shell)
         exec_echo(shell);
         return 1;
     }
-    if (my_strcmp(shell->arg_col[0], "emac") == 0) {
+    if (my_strcmp(shell->arg_col[0], "emac") == 0 ||
+        my_strcmp(shell->arg_col[0], "party-editor") == 0) {
         exec_emac(shell);
         return 1;
     }
